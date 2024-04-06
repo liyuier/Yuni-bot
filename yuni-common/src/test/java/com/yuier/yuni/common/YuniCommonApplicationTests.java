e5703376595da9ed3bc85573595a45e46b21f022
sagiri <sagiri@yui.com>
2024-04-07 00:38:46 +0800

    初始化模块

---
+++ yuni-common/src/test/java/com/yuier/yuni/common/YuniCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuier.yuni.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuniCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
