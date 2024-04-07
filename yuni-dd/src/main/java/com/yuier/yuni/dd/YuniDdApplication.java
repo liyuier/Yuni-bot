21ff3535dec063939ae73d389cc945d47495fffd
sagiri <sagiri@yui.com>
2024-04-07 20:07:47 +0800

    添加DD模块

---
+++ yuni-dd/src/main/java/com/yuier/yuni/dd/YuniDdApplication.java
@@ -0,0 +1,13 @@
+package com.yuier.yuni.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuniDdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuniDdApplication.class, args);
+    }
+
+}
---
+++ yuni-dd/src/test/java/com/yuier/yuni/dd/YuniDdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuier.yuni.dd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuniDdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
