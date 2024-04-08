a2f6b4cd1d36d1d5ccb64da63b7664549aa1c163
sagiri <sagiri@yui.com>
2024-04-09 03:53:08 +0800

    优化代码

--- yuni-core/src/main/java/com/yuier/yuni/core/controller/OneBotPostEventController.java
+++ yuni-core/src/main/java/com/yuier/yuni/core/controller/OneBotPostEventController.java
@@ -1,22 +1,19 @@
 package com.yuier.yuni.core.controller;
 
-import com.yuier.yuni.core.annotation.OneBotEventController;
 import com.yuier.yuni.core.annotation.OneBotEventHandler;
-import com.yuier.yuni.core.annotation.OneBotEventUnifiedEntrance;
+import com.yuier.yuni.core.annotation.OneBotEventEntrance;
 import com.yuier.yuni.core.domain.dto.OneBotPostEventDto;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@OneBotEventController
 @RequestMapping("/")
 public class OneBotPostEventController {
 
     @PostMapping("/")
-    @OneBotEventUnifiedEntrance
+    @OneBotEventEntrance
     public void eventUnifiedEntrance(@RequestBody OneBotPostEventDto oneBotPostEventDto) {
         System.out.println("收到无法识别事件 " + oneBotPostEventDto.getPost_type());
     }
