39b6141cf0ecfcb3d4172bf9aa9ff79c87b3140f
sagiri <sagiri@yui.com>
2024-04-09 03:41:24 +0800

    简化代码

--- yuni-core/src/main/java/com/yuier/yuni/core/aspect/EventDispatchAspect.java
+++ yuni-core/src/main/java/com/yuier/yuni/core/aspect/EventDispatchAspect.java
@@ -26,19 +26,15 @@ public class EventDispatchAspect {
         OneBotPostEventDto oneBotPostEventDto = (OneBotPostEventDto) joinPoint.getArgs()[0];
         String postType = oneBotPostEventDto.getPost_type();
 
-        // 注入带有 @OneBotEventController 注解的 Bean
-        Map<String, Object> beansWithMethods = applicationContext.getBeansWithAnnotation(OneBotEventController.class);
-        for (Object bean : beansWithMethods.values()) {
-            Method[] declaredMethods = bean.getClass().getSuperclass().getDeclaredMethods();
-            // 在 Bean 中搜索打上了 @OneBotEventHandler 注解的方法
-            for (Method method : declaredMethods) {
-                if (method.isAnnotationPresent(OneBotEventHandler.class) && method.getAnnotation(OneBotEventHandler.class).value().equals(postType)) {
-                    OneBotEventHandler annotation = method.getDeclaredAnnotation(OneBotEventHandler.class);
-                    System.out.println(annotation.value());
-                    // 使用反射调用方法
-                    method.setAccessible(true);
-                    return method.invoke(bean, oneBotPostEventDto);
-                }
+        Method[] declaredMethods = joinPoint.getTarget().getClass().getDeclaredMethods();
+        // 在 Bean 中搜索打上了 @OneBotEventHandler 注解的方法
+        for (Method method : declaredMethods) {
+            if (method.isAnnotationPresent(OneBotEventHandler.class) && method.getAnnotation(OneBotEventHandler.class).value().equals(postType)) {
+                OneBotEventHandler annotation = method.getDeclaredAnnotation(OneBotEventHandler.class);
+                System.out.println(annotation.value());
+                // 使用反射调用方法
+                method.setAccessible(true);
+                return method.invoke(joinPoint.getTarget(), oneBotPostEventDto);
             }
         }
 
