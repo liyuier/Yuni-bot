ef340d26f316cf1726fd5e8d5c20fc84caa374b2
liyuier <me@yuier.com>
2024-04-09 03:31:25 +0800

    实现根据参数的不同分派不同 Controller

--- yuni-core/src/main/java/com/yuier/yuni/core/controller/OneBotPostEventController.java
+++ yuni-core/src/main/java/com/yuier/yuni/core/controller/OneBotPostEventController.java
@@ -3,8 +3,10 @@ package com.yuier.yuni.core.controller;
 import com.yuier.yuni.core.annotation.OneBotEventController;
 import com.yuier.yuni.core.annotation.OneBotEventHandler;
 import com.yuier.yuni.core.annotation.OneBotEventUnifiedEntrance;
+import com.yuier.yuni.core.domain.dto.OneBotPostEventDto;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,28 +17,27 @@ public class OneBotPostEventController {
 
     @PostMapping("/")
     @OneBotEventUnifiedEntrance
-    public void eventUnifiedEntrance(HttpServletRequest request) {
-        System.out.println(request.getParameter("group_id"));
-        System.out.println("收到事件");
+    public void eventUnifiedEntrance(@RequestBody OneBotPostEventDto oneBotPostEventDto) {
+        System.out.println("收到无法识别事件 " + oneBotPostEventDto.getPost_type());
     }
 
     @OneBotEventHandler("message")
-    public void messageEventHandler(HttpServletRequest request) {
+    public void messageEventHandler(@RequestBody OneBotPostEventDto oneBotPostEventDto) {
         System.out.println("进入了消息事件处理逻辑");
     }
 
     @OneBotEventHandler("notice")
-    public void noticeEventHandler(HttpServletRequest request) {
+    public void noticeEventHandler(@RequestBody OneBotPostEventDto oneBotPostEventDto) {
         System.out.println("进入了通知事件处理逻辑");
     }
 
     @OneBotEventHandler("request")
-    public void requestEventHandler(HttpServletRequest request) {
+    public void requestEventHandler(@RequestBody OneBotPostEventDto oneBotPostEventDto) {
         System.out.println("进入了请求事件处理逻辑");
     }
 
     @OneBotEventHandler("meta_event")
-    public void metaEventHandler(HttpServletRequest request) {
+    public void metaEventHandler(@RequestBody OneBotPostEventDto oneBotPostEventDto) {
         System.out.println("进入了元事件处理逻辑");
     }
 
