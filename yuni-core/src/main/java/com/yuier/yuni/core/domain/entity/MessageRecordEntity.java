161c3b1a6d7302e44693739c720f46a9c721077f
sagiri <sagiri@yui.com>
2024-04-09 23:10:00 +0800

    优化 Entity 模板

--- yuni-core/src/main/java/com/yuier/yuni/core/domain/entity/MessageRecordEntity.java
+++ yuni-core/src/main/java/com/yuier/yuni/core/domain/entity/MessageRecordEntity.java
@@ -4,64 +4,98 @@ import java.util.Date;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 
 /**
  * (MessageRecord)表实体类
  *
  * @author liyuier
- * @since 2024-04-08 01:38:57
+ * @since 2024-04-09 23:00:30
  */
 @SuppressWarnings("serial")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@TableName("cms_message_record")
 public class MessageRecordEntity {
     
-    // id
+    /**
+    * id
+    */
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     
-    // 收到消息的时间戳
+    /**
+    * 收到消息的时间戳
+    */
     private Date time;
     
-    // 收到消息的机器人QQ号
+    /**
+    * 收到消息的机器人QQ号
+    */
     private Long selfId;
     
-    // 消息类型。如果是私聊消息，为 private；如果是群聊消息，为group
+    /**
+    * 消息类型。如果是私聊消息，为 private；如果是群聊消息，为group
+    */
     private String messageType;
     
-    // 消息子类型
-    // 如果是私聊消息：如果是好友则是 friend，如果是群临时会话则是 group。
-    // 如果是 群聊消息：正常消息是 normal，匿名消息是 anonymous，系统提示（如【管理员已禁止群内匿名聊天】）是 notice。
+    /**
+    * 消息子类型
+如果是私聊消息：如果是好友则是 friend，如果是群临时会话则是 group。
+如果是 群聊消息：正常消息是 normal，匿名消息是 anonymous，系统提示（如【管理员已禁止群内匿名聊天】）是 notice。
+    */
     private String subType;
     
-    // 消息ID
+    /**
+    * 消息ID
+    */
     private Long messageId;
     
-    // 群号。只有为群聊消息时有效。
+    /**
+    * 群号。只有为群聊消息时有效。
+    */
     private Long groupId;
     
-    // 发送者 QQ 号
+    /**
+    * 发送者 QQ 号
+    */
     private Long userId;
     
-    // 默认0，表示不是匿名信息；1表示是匿名信息。只有当群聊消息时有效。
+    /**
+    * 默认0，表示不是匿名信息；1表示是匿名信息。只有当群聊消息时有效。
+    */
     private Integer anonymous;
     
-    // 匿名用户ID
+    /**
+    * 匿名用户ID
+    */
     private Long anonymousId;
     
-    // 匿名用户名称
+    /**
+    * 匿名用户名称
+    */
     private String anonymousName;
     
-    // 匿名用户 flag，调用禁言API时传入
+    /**
+    * 匿名用户 flag，调用禁言API时传入
+    */
     private String anonymousFlag;
     
-    // 消息内容
+    /**
+    * 消息内容
+    */
     private String message;
     
-    // 原始消息内容
+    /**
+    * 原始消息内容
+    */
     private String rawMessage;
     
-    // 删除标志，默认0，表示未删除；1表示删除
+    /**
+    * 删除标志，默认0，表示未删除；1表示删除
+    */
     private Integer delFlag;
 }
-
