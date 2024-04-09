7a29c78426b039d4e5c4dd2abe87859f4720a11b
liyuier <me@yuier.com>
2024-04-10 00:48:45 +0800

    统一响应格式

---
+++ yuni-common/src/main/java/com/yuier/yuni/common/enums/AppHttpCodeEnum.java
@@ -0,0 +1,30 @@
+package com.yuier.yuni.common.enums;
+
+/**
+ * @Title: AppHttpCodeEnum
+ * @Author yuier
+ * @Package com.yuier.yuni.common.enums
+ * @Date 2024/4/9 23:36
+ * @description: 响应码枚举类
+ */
+public enum AppHttpCodeEnum {
+    // 成功
+    SUCCESS(200,"操作成功"),
+    SYSTEM_ERROR(500,"出现错误");
+    final int code;
+    final String msg;
+
+    // 构造方法
+    AppHttpCodeEnum(int code, String errorMessage){
+        this.code = code;
+        this.msg = errorMessage;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
