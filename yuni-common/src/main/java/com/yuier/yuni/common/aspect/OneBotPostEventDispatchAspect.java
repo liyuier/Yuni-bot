94b772804b643aee53a0c8c5c43e0e7b35680730
liyuier <me@yuier.com>
2024-04-11 23:34:01 +0800

    重构事件处理器，插件化处理器

--- yuni-common/src/main/java/com/yuier/yuni/common/aspect/OneBotPostEventDispatchAspect.java
+++ yuni-common/src/main/java/com/yuier/yuni/common/aspect/OneBotPostEventDispatchAspect.java
@@ -28,7 +28,7 @@ public class OneBotPostEventDispatchAspect {
     @Autowired
     ApplicationContext applicationContext;
 
-    // 切入点为打了 @OneBotEventUnifiedEntrance 注解的方法
+    // 切入点为打了 @OneBotPostEntrance 注解的方法
     @Around("@annotation(com.yuier.yuni.common.annotation.OneBotPostEntrance)")
     public Object dispatchBasedOnPostType(ProceedingJoinPoint joinPoint) throws Throwable {
         // 解析入参
@@ -37,6 +37,7 @@ public class OneBotPostEventDispatchAspect {
         field.setAccessible(true);
         String postType = (String) field.get(oneBotPostEventDto);
 
+        // 遍历打了 @OneBotEventHandler 注解的类
         Map<String, Object> handlerBeans = applicationContext.getBeansWithAnnotation(OneBotEventHandler.class);
         for (Object bean : handlerBeans.values()) {
             OneBotEventHandler annotation = bean.getClass().getAnnotation(OneBotEventHandler.class);
@@ -47,17 +48,6 @@ public class OneBotPostEventDispatchAspect {
                 return handle.invoke(bean, oneBotPostEventDto);
             }
         }
-//        // 通过反射，获取入口类的自定义方法
-//        Method[] declaredMethods = joinPoint.getTarget().getClass().getDeclaredMethods();
-//        // 搜索打上了 @OneBotEventHandler 注解的方法
-//        for (Method method : declaredMethods) {
-//            if (method.isAnnotationPresent(OneBotEventEntrance.class)
-//                    && method.getAnnotation(OneBotEventEntrance.class).eventType().equals(postType)) {
-//                // 使用反射调用方法
-//                method.setAccessible(true);
-//                return method.invoke(joinPoint.getTarget(), oneBotPostEventDto);
-//            }
-//        }
 
         // 如果没有找到匹配的方法，可以继续执行原始请求或者返回错误
         return joinPoint.proceed();
