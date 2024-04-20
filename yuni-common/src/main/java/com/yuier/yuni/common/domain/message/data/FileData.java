42edd0c3d2a8711b4263cb162654a6f2823e7002
liyuier <me@yuier.com>
2024-04-20 18:29:08 +0800

    适配扩展API，修改一些实体类字段； 增加枚举类，尝试重构反序列化代码

---
+++ yuni-common/src/main/java/com/yuier/yuni/common/domain/message/data/FileData.java
@@ -0,0 +1,35 @@
+package com.yuier.yuni.common.domain.message.data;
+
+import com.yuier.yuni.common.annotation.MessageDataEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Title: FileData
+ * @Author yuier
+ * @Package com.yuier.yuni.common.domain.message.data
+ * @Date 2024/4/20 17:55
+ * @description: 文件 data 字段
+ */
+@Data
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@MessageDataEntity(messageType = "file")
+public class FileData extends MessageData {
+    /**
+     * 文件路径
+     */
+    private String file;
+    /**
+     * 发送时支持自定义显示文件名
+     */
+    private String name;
+
+    @Override
+    public String toString() {
+        return "【文件" + "<file=" + this.file + ">】";
+    }
+}
---
+++ yuni-common/src/main/java/com/yuier/yuni/common/domain/message/data/MarkdownData.java
@@ -0,0 +1,28 @@
+package com.yuier.yuni.common.domain.message.data;
+
+import com.yuier.yuni.common.annotation.MessageDataEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Title: MarkdownData
+ * @Author yuier
+ * @Package com.yuier.yuni.common.domain.message.data
+ * @Date 2024/4/20 17:50
+ * @description: markdown 消息 data 字段
+ */
+@Data
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@MessageDataEntity(messageType = "markdown")
+public class MarkdownData extends MessageData {
+    private String data;
+
+    @Override
+    public String toString() {
+        return "【markdown消息<#" + this.data + ">】";
+    }
+}
