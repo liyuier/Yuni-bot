cf25894114a5622833f374ee8f7f2e974fb170fe
sagiri <sagiri@yui.com>
2024-04-21 14:37:22 +0800

    优化日志打印

--- yuni-common/src/main/java/com/yuier/yuni/common/annotation/function/OrderCallFunction.java
+++ yuni-common/src/main/java/com/yuier/yuni/common/annotation/function/OrderCallFunction.java
@@ -15,5 +15,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OrderCallFunction {
-    String orderWord();
+    String orderWord() default "";
+
 }
