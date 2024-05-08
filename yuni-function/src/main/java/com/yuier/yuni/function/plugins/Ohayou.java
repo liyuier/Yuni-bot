e441605da408569a6ab914de88994e6650d1f556
liyuier <me@yuier.com>
2024-05-09 01:10:32 +0800

    新增早安功能、意见收集功能

--- yuni-function/src/main/java/com/yuier/yuni/function/plugins/Ohayou.java
+++ yuni-function/src/main/java/com/yuier/yuni/function/plugins/Ohayou.java
@@ -34,7 +34,7 @@ public class Ohayou implements PositivePlugin {
     }
 
     @Override
-    @Scheduled(cron = "30 8 * * *")
+    @Scheduled(cron = "30 8 * * * ?")
     public void run() {
         Long[] groups = new Long[] {930198267L, 287900567L};
         for (Long groupId : groups) {
