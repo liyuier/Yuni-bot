2c29fab30230611296b3fb1e26025062322c2e2d
liyuier <me@yuier.com>
2024-05-29 22:16:07 +0800

    当触发订阅 UP 插件时，如果没有关注 UP ，关注；如果没有保存 UP 基础信息，保存

---
+++ yuni-dd/src/main/java/com/yuier/yuni/dd/mapper/UperFollowedMapper.java
@@ -0,0 +1,15 @@
+package com.yuier.yuni.dd.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuier.yuni.dd.domain.entity.UperFollowedEntity;
+
+/**
+ * (UperFollowed)表数据库访问层
+ *
+ * @author liyuier
+ * @since 2024-05-28 23:46:10
+ */
+public interface UperFollowedMapper extends BaseMapper<UperFollowedEntity> {
+
+}
+
---
+++ yuni-dd/src/main/java/com/yuier/yuni/dd/service/UperFollowedService.java
@@ -0,0 +1,15 @@
+package com.yuier.yuni.dd.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yuier.yuni.dd.domain.entity.UperFollowedEntity;
+
+/**
+ * (UperFollowed)表服务接口
+ *
+ * @author liyuier
+ * @since 2024-05-28 23:46:10
+ */
+public interface UperFollowedService extends IService<UperFollowedEntity> {
+
+}
+
